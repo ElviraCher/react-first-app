fe48a391ac0cae8a51b6b784dab3de6dada7f990
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-01-21 18:29:02 +0300

    Add story for header

---
+++ src/Header/Header.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Header } from './Header';
+
+export default {
+    title: 'Header component',
+    component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    user: {},
+};
