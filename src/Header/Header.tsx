39afbc185cc5d94eb72e02dfebc16c28032a1066
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-01-27 14:46:43 +0300

    Change count of size variants

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -9,9 +9,6 @@ interface HeaderProps {
 
 export function Header(props: HeaderProps) {
   let size = Math.round(+props.size).toString();
-  if (props.size !== size) {
-    size = "1";
-  }
 
   switch (size) {
     case "1":
@@ -26,31 +23,11 @@ export function Header(props: HeaderProps) {
           {props.text}
         </h2>
       );
-    case "3":
+    default:
       return (
         <h3 className="header" onClick={props.headerClick} data-testid="header">
           {props.text}
         </h3>
       );
-    case "4":
-      return (
-        <h4 className="header" onClick={props.headerClick} data-testid="header">
-          {props.text}
-        </h4>
-      );
-    case "5":
-      return (
-        <h5 className="header" onClick={props.headerClick} data-testid="header">
-          {props.text}
-        </h5>
-      );
-    case "6":
-      return (
-        <h6 className="header" onClick={props.headerClick} data-testid="header">
-          {props.text}
-        </h6>
-      );
-    default:
-      return null;
   }
 }
