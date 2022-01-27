652f5a5703b5fe3b2746948d8f9328c6b7e7514f
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-01-27 14:55:04 +0300

    Change by Eslint

--- src/SmartComponent/SmartComponent.tsx
+++ src/SmartComponent/SmartComponent.tsx
@@ -10,117 +10,127 @@ import { CollapsingBlock } from "../CollapsingBlock/CollapsingBlock.tsx";
 import "./SmartComponent.css";
 
 export function SmartComponent() {
-    const [headerText, setHeaderText] = useState("");
-    const [headerSize, setHeaderSize] = useState("");
-    const [paragraphStyle, setParagraphStyle] = useState("normal");
-    const [collapsingBlockText, setCollapsingBlockText] = useState("");
-    const [paragraphText, setParagraphText] = useState("");
-    const [isShown, setIsShown] = useState(true);
-    const [columnCount, setColumnCount] = useState("");
-    const [columnContent, setColumnContent] = useState<string[]>([]);
-    const [imageFloat, setImageFloat] = useState("none");
-    const [imageAtThePage, setImageAtThePage] = useState(false);
+  const [headerText, setHeaderText] = useState("");
+  const [headerSize, setHeaderSize] = useState("");
+  const [paragraphStyle, setParagraphStyle] = useState("normal");
+  const [collapsingBlockText, setCollapsingBlockText] = useState("");
+  const [paragraphText, setParagraphText] = useState("");
+  const [isShown, setIsShown] = useState(true);
+  const [columnCount, setColumnCount] = useState("");
+  const [columnContent, setColumnContent] = useState<string[]>([]);
+  const [imageFloat, setImageFloat] = useState("none");
+  const [imageAtThePage, setImageAtThePage] = useState(false);
 
-    function finishPrompt(data: string) {
-        return data;
+  function finishPrompt(data: string) {
+    return data;
+  }
+
+  function checkPrompt(
+    callback: (data: string) => string,
+    promptMessage: string | null
+  ) {
+    let x = prompt(`${promptMessage}`);
+
+    while (x === null) {
+      x = checkPrompt(finishPrompt, promptMessage);
     }
+    return callback(x);
+  }
 
-    function checkPrompt(
-        callback: (data: string) => string,
-        promptMessage: string | null
-    ) {
-        let x = prompt(`${promptMessage}`);
+  const headerSizePrompt =
+    "Введите размер заголовка: 1 - большой, 2 - средний, 3 - маленький";
+  const headerTextPrompt = "Введите текст заголовка";
+  const paragraphStylePrompt =
+    "Введите стиль параграфа. Цифра '0' - обычный; '1' - курсив; '2'- жирный";
+  const paragraphTextPrompt = "Введите текст параграфа";
+  const columnsCountPrompt = "Сколько колонок будет на странице?";
+  const columnsContentPrompt = "Введите текст колонки";
+  const imageFloatPrompt =
+    "Выберете выравнивание картинки: '0' - по левому краю; '1' - по правому краю; '2' - нет обтекания";
 
-        while (x === null) {
-            x = checkPrompt(finishPrompt, promptMessage);
-        }
-        return callback(x);
+  const headerHandler = () => {
+    const size = checkPrompt(finishPrompt, headerSizePrompt);
+    const header = checkPrompt(finishPrompt, headerTextPrompt);
+    setHeaderText(header);
+    setHeaderSize(size);
+  };
+
+  const headerClick = () => {
+    setIsShown(!isShown);
+  };
+
+  const collapsingBlockHandler = () => {
+    const text = checkPrompt(finishPrompt, paragraphTextPrompt);
+    setCollapsingBlockText(text);
+  };
+
+  const columnHandler = () => {
+    const count = checkPrompt(finishPrompt, columnsCountPrompt);
+    let content;
+    let contentArray: [] | string[] = [];
+
+    for (let i = +count; i > 0; i -= 1) {
+      content = checkPrompt(finishPrompt, columnsContentPrompt);
+      contentArray = [...contentArray, content];
     }
+    setColumnCount(count);
+    setColumnContent(contentArray);
+  };
 
-    const headerSizePrompt = "Введите размер заголовка: 1 - большой, 2 - средний, 3 - маленький";
-    const headerTextPrompt = "Введите текст заголовка";
-    const paragraphStylePrompt =
-        "Введите стиль параграфа. Цифра '0' - обычный; '1' - курсив; '2'- жирный";
-    const paragraphTextPrompt = "Введите текст параграфа";
-    const columnsCountPrompt = "Сколько колонок будет на странице?";
-    const columnsContentPrompt = "Введите текст колонки";
-    const imageFloatPrompt = "Выберете выравнивание картинки: '0' - по левому краю; '1' - по правому краю; '2' - нет обтекания";
+  const paragraphHandler = () => {
+    const style = checkPrompt(finishPrompt, paragraphStylePrompt);
+    const text = checkPrompt(finishPrompt, paragraphTextPrompt);
+    setParagraphStyle(style);
+    setParagraphText(text);
+  };
 
-    const headerHandler = () => {
-        const size = checkPrompt(finishPrompt, headerSizePrompt);
-        const header = checkPrompt(finishPrompt, headerTextPrompt);
-        setHeaderText(header);
-        setHeaderSize(size);
-    };
+  const imageHandler = () => {
+    setImageAtThePage(true);
+    const float = checkPrompt(finishPrompt, imageFloatPrompt);
+    float === "0"
+      ? setImageFloat("left")
+      : float === "1"
+      ? setImageFloat("right")
+      : setImageFloat("none");
+  };
 
-    const headerClick = () => {
-        setIsShown(!isShown);
-    };
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button className="button" onClick={headerHandler}>
+          {headerText ? "Изменить заголовок" : "Установить заголовок"}
+        </button>
+        <button className="button" onClick={collapsingBlockHandler}>
+          {collapsingBlockText
+            ? "Изменить подзаголовок"
+            : "Установить подзаголовок"}
+        </button>
+        <button className="button" onClick={paragraphHandler}>
+          {paragraphText ? "Изменить параграф" : "Добавить параграф"}
+        </button>
+        <button className="button" onClick={columnHandler}>
+          {columnCount ? "Изменить колонки" : "Добавить колонки"}
+        </button>
+        <button className="button" onClick={imageHandler}>
+          {imageAtThePage ? "Изменить обтекание картинки" : "Добавить картинку"}
+        </button>
+      </div>
 
-    const collapsingBlockHandler = () => {
-        const text = checkPrompt(finishPrompt, paragraphTextPrompt);
-        setCollapsingBlockText(text);
-    };
+      <div className="header-container">
+        <Header size={headerSize} text={headerText} headerClick={headerClick} />
+        <CollapsingBlock text={collapsingBlockText} isShawn={isShown} />
+      </div>
 
-    const columnHandler = () => {
-        const count = checkPrompt(finishPrompt, columnsCountPrompt);
-        let content;
-        let contentArray:[] | string[] = [];
-
-        for (let i = +count; i > 0; i -= 1) {
-            content = checkPrompt(finishPrompt, columnsContentPrompt);
-            contentArray = [...contentArray, content];
-        }
-        setColumnCount(count);
-        setColumnContent(contentArray);
-    };
-
-    const paragraphHandler = () => {
-        const style = checkPrompt(finishPrompt, paragraphStylePrompt);
-        const text = checkPrompt(finishPrompt, paragraphTextPrompt);
-        setParagraphStyle(style);
-        setParagraphText(text);
-    };
-
-    const imageHandler = () => {
-        setImageAtThePage(true);
-        const float = checkPrompt(finishPrompt, imageFloatPrompt);
-        float === "0" ? setImageFloat("left") : float === "1" ? setImageFloat("right") : setImageFloat("none");
-    };
-
-    return (
-        <div className="container">
-            <div className="button-container">
-                <button className="button"onClick={headerHandler}>{headerText ? "Изменить заголовок": "Установить заголовок"}</button>
-                <button className="button"onClick={collapsingBlockHandler}>
-                    {collapsingBlockText ? "Изменить подзаголовок": "Установить подзаголовок"}
-                </button>
-                <button className="button"onClick={paragraphHandler}>{paragraphText ? "Изменить параграф" : "Добавить параграф"}</button>
-                <button className="button"onClick={columnHandler}>{columnCount ? "Изменить колонки" : "Добавить колонки"}</button>
-                <button className="button"onClick={imageHandler}>
-                    {imageAtThePage ? "Изменить обтекание картинки" : "Добавить картинку"}
-                </button>
-            </div>
-
-            <div className="header-container">
-                <Header size={headerSize} text={headerText} headerClick={headerClick} />
-                <CollapsingBlock text={collapsingBlockText} isShawn={isShown} />
-            </div>
-
-
-
-            <Column
-                columnCount={columnCount}
-                columnContent={columnContent}
-                paragraphTextStyle="0"
-            />
-            <SpaceBlock />
-            <div className="main">
-                <>{imageAtThePage ? <Image imageFloat={imageFloat} /> : null}</>
-                <Paragraph textStyle={paragraphStyle} text={paragraphText} />
-            </div>
-
-        </div>
-    );
+      <Column
+        columnCount={columnCount}
+        columnContent={columnContent}
+        paragraphTextStyle="0"
+      />
+      <SpaceBlock />
+      <div className="main">
+        <>{imageAtThePage ? <Image imageFloat={imageFloat} /> : null}</>
+        <Paragraph textStyle={paragraphStyle} text={paragraphText} />
+      </div>
+    </div>
+  );
 }
-
