927932fc29bc79bfe1b5e76b8ed40d390cd66984
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-01-28 14:44:14 +0300

    Add data-testid

--- src/Paragraph/Paragraph.tsx
+++ src/Paragraph/Paragraph.tsx
@@ -9,10 +9,22 @@ interface ParagraphProps {
 export function Paragraph(props: ParagraphProps) {
   switch (props.textStyle) {
     case "1":
-      return <i className="paragraph">{props.text}</i>;
+      return (
+        <i className="paragraph" data-testid="paragraph-italic">
+          {props.text}
+        </i>
+      );
     case "2":
-      return <b className="paragraph">{props.text}</b>;
+      return (
+        <b className="paragraph" data-testid="paragraph-bold">
+          {props.text}
+        </b>
+      );
     default:
-      return <p className="paragraph">{props.text}</p>;
+      return (
+        <p className="paragraph" data-testid="paragraph-normal">
+          {props.text}
+        </p>
+      );
   }
 }
